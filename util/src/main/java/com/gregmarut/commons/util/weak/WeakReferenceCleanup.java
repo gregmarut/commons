8d93ee98db4e230446481982cf29504450127ef8
Greg Marut <greg.marut@gmail.com>
2017-08-02 08:56:39 -0400

    added synchronization to the weak reference list

--- util/src/main/java/com/gregmarut/commons/util/weak/WeakReferenceCleanup.java
+++ util/src/main/java/com/gregmarut/commons/util/weak/WeakReferenceCleanup.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Greg Marut - initial API and implementation
  * </pre>
@@ -18,14 +18,14 @@ import java.util.List;
 /**
  * This class allows for handling clean up code on any object that is still remaining in memory. Objects are held with a
  * weak reference and can be GCed at any time without notifying this object
- * 
- * @author Greg Marut
+ *
  * @param <T>
+ * @author Greg Marut
  */
 public abstract class WeakReferenceCleanup<T>
 {
 	// holds the list of objects
-	private List<EquatableWeakReference<T>> weakObjects;
+	private final List<EquatableWeakReference<T>> weakObjects;
 	
 	public WeakReferenceCleanup()
 	{
@@ -34,25 +34,34 @@ public abstract class WeakReferenceCleanup<T>
 	
 	public void add(T object)
 	{
-		weakObjects.add(new EquatableWeakReference<T>(object));
+		synchronized (weakObjects)
+		{
+			weakObjects.add(new EquatableWeakReference<T>(object));
+		}
 	}
 	
 	public boolean remove(T object)
 	{
-		return weakObjects.remove(object);
+		synchronized (weakObjects)
+		{
+			return weakObjects.remove(object);
+		}
 	}
 	
 	public void clear()
 	{
-		weakObjects.clear();
+		synchronized (weakObjects)
+		{
+			weakObjects.clear();
+		}
 	}
 	
 	/**
 	 * Cleans up any weak references that are still in memory
-	 * 
+	 *
 	 * @return the number of objects that were cleaned up
 	 */
-	public final synchronized int cleanUp()
+	public final int cleanUp()
 	{
 		// holds the list of objects to remove
 		List<EquatableWeakReference<T>> weakObjectsToRemove = new ArrayList<EquatableWeakReference<T>>();
@@ -60,35 +69,38 @@ public abstract class WeakReferenceCleanup<T>
 		// holds the number of objects that were cleaned up
 		int count = 0;
 		
-		// for each of the objects
-		for (EquatableWeakReference<T> weakObject : weakObjects)
+		synchronized (weakObjects)
 		{
-			// make sure the object is not null
-			if (null != weakObject)
+			// for each of the objects
+			for (EquatableWeakReference<T> weakObject : weakObjects)
 			{
-				// get a hard reference to the object
-				T object = weakObject.get();
-				
 				// make sure the object is not null
-				if (null != object)
+				if (null != weakObject)
 				{
-					// clean up this object
-					cleanUp(object);
-					count++;
-				}
-				else
-				{
-					// need to remove this from the list
-					weakObjectsToRemove.add(weakObject);
+					// get a hard reference to the object
+					T object = weakObject.get();
+					
+					// make sure the object is not null
+					if (null != object)
+					{
+						// clean up this object
+						cleanUp(object);
+						count++;
+					}
+					else
+					{
+						// need to remove this from the list
+						weakObjectsToRemove.add(weakObject);
+					}
 				}
 			}
-		}
-		
-		// check to see if there are objects to remove
-		if (!weakObjectsToRemove.isEmpty())
-		{
-			// remove these objects from the list
-			weakObjects.removeAll(weakObjectsToRemove);
+			
+			// check to see if there are objects to remove
+			if (!weakObjectsToRemove.isEmpty())
+			{
+				// remove these objects from the list
+				weakObjects.removeAll(weakObjectsToRemove);
+			}
 		}
 		
 		return count;
