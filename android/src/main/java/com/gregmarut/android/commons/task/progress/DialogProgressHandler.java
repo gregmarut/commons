c974180607a2acf38dd8ab28cb5dc6d62bc5a76c
Greg Marut <greg.marut@gmail.com>
2019-06-27 21:04:27 -0400

    catching a runtime exception

--- android/src/main/java/com/gregmarut/android/commons/task/progress/DialogProgressHandler.java
+++ android/src/main/java/com/gregmarut/android/commons/task/progress/DialogProgressHandler.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
  *     Greg Marut - initial API and implementation
  * </pre>
@@ -60,9 +60,16 @@ public class DialogProgressHandler implements ProgressHandler
 	public void end()
 	{
 		// make sure the progress bar has not been destroyed
-		if (null != progress)
+		if (null != progress && progress.isShowing())
 		{
-			progress.dismiss();
+			try
+			{
+				progress.dismiss();
+			}
+			catch (Exception e)
+			{
+				//ignore
+			}
 		}
 	}
 	
